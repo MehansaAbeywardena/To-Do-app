7e584e3ee461c76396fdbf32144d2976c76970de
Mehansa Abeywardena <orshadhi@gmail.com>
2024-05-15 18:34:20 +0530

    Backend Functionality Completed and Frontend Folder Created

--- backend/controllers/ToDoController.js
+++ backend/controllers/ToDoController.js
@@ -5,34 +5,33 @@ module.exports.getToDo = async (req, res) => {
     res.send(todo)
 }
 
-module.exports.saveToDo = async(req,res) => {
-    const {text} = req.body
+module.exports.saveToDo = async (req, res) => {
+    const { text } = req.body
 
-    ToDoSchema.create({text})
-    .then((data)=>{
-        console.log("Added the task Successfuly!");
-        console.log(data);
-        res.send(data)
-    })
-    
+    ToDoSchema.create({ text })
+        .then((data) => {
+            console.log("Added the task Successfuly!");
+            console.log(data);
+            res.send(data)
+        })
+
 }
 
-module.exports.updateToDo = async(req, res)=>{
-    const {_id, text} = req.body
-    ToDoModel
-    .updateById(_id, {text})
-    .then(()=>{
-        res.send("Task Successfully Updated!")
-        .catch((err)=>console.log(err))
-    })
+module.exports.updateToDo = async (req, res) => {
+    const { _id, text } = req.body
+    ToDoSchema
+        .findByIdAndUpdate(_id, { text })
+        .then(() =>
+            res.send("Task Successfully Updated!"))
+        .catch((err) => console.log(err))
 }
 
-module.exports.deleteToDo = async(req, res)=>{
-    const {_id, text} = req.body
-    ToDoModel
-    .deleteByID(_id, {text})
-    .then(()=>{
-        res.send("Task Successfully Deleted!")
-        .catch((err)=>console.log(err))
-    })
+module.exports.deleteToDo = async (req, res) => {
+    const { _id, text } = req.body
+    ToDoSchema
+        .findByIdAndDelete(_id)
+        .then(() =>
+            res.send("Task Successfully Deleted!"))
+        .catch((err) => console.log(err))
+
 }
